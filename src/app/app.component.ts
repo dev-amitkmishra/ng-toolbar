6c52a6ada61ac74a6c8361b5b572b9cc842ffb6e
Amit Mishra <dev.amitkmishra@gmail.com>
2019-05-07 21:06:13 +0530

    dynamic component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewContainerRef, AfterViewInit, ViewChild } from '@angular/core';
+import { DynamicComponent } from './dynamic/dynamic.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-toolbar';
+export class AppComponent implements AfterViewInit {
+    @ViewChild('dynamicInsert', { read: ViewContainerRef }) dynamicInsert: ViewContainerRef;
+    title = 'ng-toolbar';
+    constructor(private viewContainerRef: ViewContainerRef, private componentFactoryResolver: ComponentFactoryResolver) {}
+
+    ngAfterViewInit() {
+        const componentFactory = this.componentFactoryResolver.resolveComponentFactory(DynamicComponent);
+        this.dynamicInsert.clear();
+        const dyynamicComponent: any = this.dynamicInsert.createComponent(componentFactory).instance;
+        dyynamicComponent.someProp = 'Hello World';
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppService } from './base/appService';
+import { DynamicComponent } from './dynamic/dynamic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DynamicComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  entryComponents: [DynamicComponent],
   providers: [AppService],
   bootstrap: [AppComponent]
 })
